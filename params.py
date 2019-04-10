ef6792c7ffd6c9909d7b03410ad54f520bc07416
DmitriiGudin <49461403+DmitriiGudin@users.noreply.github.com>
2019-04-09 21:31:19 -0400

    Add files via upload

--- params.py
+++ params.py
@@ -2,6 +2,6 @@ from __future__ import division
 import numpy as np
 
 
-spectral_grid_file = "spectra_raw.hdf5"
 Shepard_parameter = 2
+spectral_grid_file = "R2000_clean_normed.hdf5"
 output_directory = "output"
