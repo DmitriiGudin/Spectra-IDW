7d7eb441c05719aa72a4ece0d2eec612f7350483
DmitriiGudin <49461403+DmitriiGudin@users.noreply.github.com>
2019-04-15 17:17:13 -0400

    Add files via upload

--- params.py
+++ params.py
@@ -5,6 +5,6 @@ import numpy as np
 Shepard_parameter = 2
 
 
-spectral_grid_file = "R2000_clean_normed.hdf5"
+spectral_grid_file = "R2000_clean_normed_cut.hdf5"
 param_vals_file = "param_vals.csv"
 output_directory = "output"
